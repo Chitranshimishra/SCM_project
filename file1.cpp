400461f9329e99fc44161ebc914b8d1e1608743d
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2019-04-22 15:39:00 +0530

    commit3

--- file1.cpp
+++ file1.cpp
@@ -327,3 +327,30 @@ case 7:
 case 8:
 case 10:
 case 12:
+{
+cin>>DOB11[temp].date;
+while(DOB11[temp].date<1||DOB11[temp].date>31)
+{
+clreol();
+cout<<"\nInvalid date !"<<"";
+cout<<"\nAgain enter the date: "<<"";
+clreol();
+cin>>DOB11[temp].date;
+}
+break;
+}
+case 2:
+{
+cin>>DOB11[temp].date;
+if(DOB11[temp].year%4==0)
+{
+while(DOB11[temp].date<0||DOB11[temp].date>29)
+//for leap year
+{
+clreol();
+cout<<"\nInvalid date !"<<"";
+cout<<"\nAgain enter the date: "<<"";
+clreol();
+cin>>DOB11[temp].date;
+}
+}	
