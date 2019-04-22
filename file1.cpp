3db8aef5202b9afc7a2d416b7aee09e317104f4e
Chitranshimishra <chitranshimishra2000@gmail.com>
2019-04-22 14:53:28 +0530

    commit2 to HMS

--- file1.cpp
+++ file1.cpp
@@ -41,3 +41,48 @@ char answer1;
 char ch;
 int serial;
 };
+class date
+{
+private:
+int date;
+int month;
+int year;
+public:
+void enter_date();
+void show_date();
+};
+class dob
+{
+private:
+struct dob1
+{
+int date;
+int month;
+int year;
+int rem;
+}
+DOB11[100];
+public:
+void enter_date();
+void show_date();
+};
+int i=0;
+int rem;
+int count;
+int regis;
+int attempt;
+int temp;
+int show_count=0;
+all A1; //object declared
+date D1; //object declared
+dob DOB1; //object declared
+void main()
+{
+clrscr();
+count=0;
+cout<<"Welcome to..."<<"";
+cout<<"\n\t\t***HOSPITAL MANAGEMENT SOFTWARE***"<<"";
+D1.enter_date();
+A1.tasks();
+getch();
+};
